7d7488afe1e1d3da1ddc075a839a27adb400151f
ArbitraryRage <jacksonean@twc.com>
2017-02-27 23:45:34 -0500

    test

--- src/main/java/com/waabbuffet/kotrt/entities/KingdomAI/BuilderAI.java
+++ src/main/java/com/waabbuffet/kotrt/entities/KingdomAI/BuilderAI.java
@@ -24,7 +24,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityChest;
-import net.minecraft.util.BlockPos;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BuilderAI extends EntityAIBase{
